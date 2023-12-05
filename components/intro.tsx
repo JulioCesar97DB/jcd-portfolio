114a9137ef6e001d4bd7d4ca68968d39e59cc215
JCD <juliocesar97db@gmail.com>
2023-12-05 15:25:55 -0500

    refactorizing

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,28 +1,17 @@
 'use client'
 
 import Image from 'next/image'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
-import { useInView } from 'react-intersection-observer'
 import { BsArrowRight, BsLinkedin } from 'react-icons/bs'
 import { HiDownload } from 'react-icons/hi'
 import { FaGithubSquare } from 'react-icons/fa'
-import { useActiveSectionContext } from '@/context/active-section-context'
+import { useSectionInView } from '@/lib/hooks'
 
 export default function Intro() {
 
-    const { ref, inView } = useInView({
-    threshold: 0.75
-  });
-    const { setActiveSection, timeOfLastClick } = useActiveSectionContext()
-
-    useEffect(() => {
-        
-        if (inView && Date.now() - timeOfLastClick > 1000) {
-            setActiveSection('Home');
-        }
-    }, [inView, setActiveSection, timeOfLastClick])
+    const { ref } = useSectionInView('Home', 0.5)
 
     return (
         <section ref={ref} id='home' className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-28'>
