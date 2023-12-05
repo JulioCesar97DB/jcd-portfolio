41ec5d31e4d534770bb6828d8ce8321eb9f0117e
JCD <juliocesar97db@gmail.com>
2023-12-05 13:42:30 -0500

    heading smooth nav and some viasual updates

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
 import SectionHeading from './section-heading'
 import { projectsData } from '@/lib/data'
+import { useInView } from 'react-intersection-observer'
 
 import Project from './project';
+import { useActiveSectionContext } from '@/context/active-section-context';
 
 export default function Projects() {
+
+  const { ref, inView } = useInView({
+    threshold: 0.4
+  });
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext()
+
+  useEffect(() => {
+
+    if (inView && Date.now() - timeOfLastClick > 1000) {
+      setActiveSection('Projects');
+    }
+  }, [inView, setActiveSection, timeOfLastClick])
+
   return (
-    <section id='projects' className='scroll-mt-28'>
+    <section ref={ref} id='projects' className='scroll-mt-28'>
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {
